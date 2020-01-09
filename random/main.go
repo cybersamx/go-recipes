cf7257f84af2e038118b37be0cd0b40e10012606
Samuel Chow <cybersamx@gmail.com>
2020-01-08 17:07:21 -0800

    Add recipe for generating random data

---
+++ random/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cybersamx/go-recipes/random/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed()
+
+	for {
+		i   := rand.RandomIntRange(-100, 100)
+		ii  := rand.RandomInt(150)
+		f   := rand.RandomFloatRange(-100, 100)
+		ff  := rand.RandomFloat(150)
+		str := rand.RandomString(10)
+		fmt.Printf("i   = %d\nii  = %d\nf   = %.6f\nff  = %.6f\ns   = %s\n\n", i, ii, f, ff, str)
+
+		time.Sleep(1 * time.Second)
+	}
+}
