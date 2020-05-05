183fecc0f01dd726302c61ac71f8a7b29ba577cc
Samuel Chow <cybersamx@gmail.com>
2020-05-04 22:59:46 -0700

    Add recipe file reader

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A collection of simple Golang recipes.
 * [Dependency injection](dependency-injection) - Sample code that demonstrates 3 different flavors of dependency injection in Golang using direct implementation and third party frameworks.
 * [Ephemeral SQL data model](ephemeral-sql-data) - Sample code of a garbage collector that removes an expired data object in a SQL database.
 * [Fake vs mock in unit testing](fake-mock) - Unit test sample using fake and mock.
+* [File reader](file-reader) - Sample code for reading a file using an all-read and an incremental approach.
 * [gRPC](grpc) - Sample code demonstrating a simple gRPC setup comprising of a client communicating with a server.
 * [Kafka pubsub](kafka-pubsub) - Kafka publisher and subscriber code using the Watermill library.
 * [Long polling](long-poll) - Sample code demonstrating long-polling service.
---
+++ file-reader/main.go
@@ -0,0 +1,71 @@
+// Modified from the official gRPC example: google.golang.org/grpc/examples/helloworld/helloworld.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// naivePrint prints the content of the file directly using ReadAll in 1 pass.
+func naivePrint(reader io.Reader) (io.ReadCloser, error) {
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(data)
+
+	return ioutil.NopCloser(bytes.NewReader(data)), nil
+}
+
+// efficientPrint breaks the reading of a stream by chunks. This is useful for
+// reading big files.
+func efficientPrint(reader io.Reader) (io.ReadCloser, error) {
+	data := make([]byte, 256)
+	for {
+		n, err := reader.Read(data)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error reading file: %v", err)
+		}
+
+		if n > 0 {
+			fmt.Print(data[:n])
+		}
+	}
+
+	return ioutil.NopCloser(bytes.NewReader(data)), nil
+}
+
+func main() {
+	// Create a file, which is an "implementation" of io.Reader.
+	filename := "testdata.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("can't open file %s: %v", filename, err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	fmt.Println("--- Naive Print ---")
+	closer, err := naivePrint(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("--- Efficient Print ---")
+	_, err = efficientPrint(closer)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
