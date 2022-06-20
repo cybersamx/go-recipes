b8000355d5356b057df310a07bf0242188812cd2
Samuel Chow <cybersamx@gmail.com>
2022-06-20 00:58:40 -0700

    Refactor cobra-cmd recipe

--- cli/cobra-cmd/commands/root.go
+++ cli/cobra-cmd/commands/root.go
@@ -2,19 +2,44 @@ package commands
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-func NewRootCommand() *cobra.Command {
-	cmd := cobra.Command{
-		Use:   "myctl",
-		Short: "myctl manages my cluster of machines.",
-		Long:  "myctl manages my cluster of machines.",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("app")
-		},
-	}
+var rootCmd = cobra.Command{
+	Use:   "myctl",
+	Short: "myctl manages my cluster of machines.",
+	Long:  "myctl manages my cluster of machines.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("myctl")
+	},
+}
 
-	return &cmd
+func initConfig() {
+	// Set up environment variables
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("CYBER")
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Global flags
+	rootCmd.PersistentFlags().String("format", "yaml", "format of the output")
+	rootCmd.PersistentFlags().Bool("debug", false, "enable debugging")
+	rootCmd.PersistentFlags().String("key", "", "key to connect to cluster")
+	cobra.CheckErr(viper.BindPFlag("format", rootCmd.PersistentFlags().Lookup("format")))
+	cobra.CheckErr(viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug")))
+	cobra.CheckErr(viper.BindPFlag("key", rootCmd.PersistentFlags().Lookup("key")))
+	viper.SetDefault("format", "yaml")
+	viper.SetDefault("debug", false)
+	viper.SetDefault("key", "")
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	cobra.CheckErr(err)
 }
--- cli/cobra-cmd/commands/run.go
+++ cli/cobra-cmd/commands/run.go
@@ -7,21 +7,21 @@ import (
 	"github.com/spf13/viper"
 )
 
-func NewRunCommand() *cobra.Command {
-	cmd := cobra.Command{
-		Use:   "run",                        // Name of the subcommand
-		Short: "Run a task on the cluster.", // Short help descriptor in the usage table in the output of myapp --help
-		Long:  "Enter author's name",        // Long help description in the output of myapp run --help
-		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println(
-				"run",
-				args,
-				viper.GetString("format"),
-				viper.GetBool("debug"),
-				viper.GetString("key"))
-			return nil
-		},
-	}
+var runCmd = cobra.Command{
+	Use:   "run",                        // Name of the subcommand
+	Short: "Run a task on the cluster.", // Short help descriptor in the usage table in the output of myapp --help
+	Long:  "Enter author's name",        // Long help description in the output of myapp run --help
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println(
+			"run",
+			args,
+			viper.GetString("format"),
+			viper.GetBool("debug"),
+			viper.GetString("key"))
+		return nil
+	},
+}
 
-	return &cmd
+func init() {
+	rootCmd.AddCommand(&runCmd)
 }
