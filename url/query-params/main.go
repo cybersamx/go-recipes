1bb7f4863ae691f1841a3f8233f0bd2b6b383c24
Samuel Chow <cybersamx@gmail.com>
2023-01-08 21:38:47 -0800

    Expand the query-params example

--- url/query-params/main.go
+++ url/query-params/main.go
@@ -11,7 +11,7 @@ func main() {
 		panic(err)
 	}
 
-	// This won't work.
+	// This won't work. Need to run Encode to get the encoded query params string.
 	apiURL.Query().Set("sort", "id")
 	apiURL.Query().Set("index", "3")
 	apiURL.Query().Set("page_size", "10")
@@ -19,6 +19,8 @@ func main() {
 	// Output: https://localhost:8000/api/v3/location/3a/shelf/5ff
 	fmt.Println(apiURL)
 
+	// A key in query params can have multiple values. In the following, the sort key has
+	// values `id` and `mod_time`.
 	query := apiURL.Query()
 	query.Set("sort", "id")
 	query.Set("index", "3")
@@ -30,4 +32,15 @@ func main() {
 
 	// Output: `https://localhost:8000/api/v3/location/3a/shelf/5ff?index=3&page_size=10&sort=id&sort=mod_time
 	fmt.Println(apiURL)
+
+	// Decoding just the query string, ie. index=3&page_size=10&sort=id&sort=mod_time
+	fmt.Println(apiURL.RawQuery)
+	params, err := url.ParseQuery(apiURL.RawQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	for k, v := range params {
+		fmt.Println(k, v)
+	}
 }
