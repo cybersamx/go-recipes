ebff37552fbcf8077e3571ef54755536af9c9682
Samuel Chow <cybersamx@gmail.com>
2023-01-20 12:15:18 -0800

    Clean up tls/postgres example

--- tls/client-postgres/main.go
+++ tls/client-postgres/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	// If we want to use the pq driver, rename the driver to 'postgres` and use SAN
 	// in the cert's CN.
 	//_ "github.com/lib/pq"
@@ -29,14 +30,19 @@ func noTLSConnect() (*sql.DB, error) {
 func tlsConnect() (*sql.DB, error) {
 	// Let's connect to postgres using tls.
 
-	// TODO: It works. But still can't use sslmode=verify-full. Self-signed CA is a pain, but running the same dsn on psql works. So probably more configuration.
-	db, err := sql.Open(driver, "host=localhost port=5432 user=postgres password=password sslmode=verify-ca sslrootcert=docker/postgresql/ca-cert.pem sslcert=docker/postgresql/client-cert.pem sslkey=docker/postgresql/client-key.pem")
+	// TODO: The dsn for postgres works now. However, we still can't use sslmode=verify-full. Self-signed CA is a pain. Using sslmode=verify-ca for now.
+	db, err := sql.Open(driver, "host=localhost port=5432 user=postgres sslmode=verify-ca sslrootcert=docker/postgresql/ca-cert.pem sslcert=docker/postgresql/client-cert.pem sslkey=docker/postgresql/client-key.pem")
 	if err != nil {
 		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
+		pgErr, ok := err.(*pgconn.PgError)
+		if !ok {
+			fmt.Println(pgErr)
+		}
+
 		return nil, err
 	}
 
@@ -47,7 +53,7 @@ func main() {
 	db, err := noTLSConnect()
 	if err != nil {
 		fmt.Println(err)
-		fmt.Println("This error is expected when connecting to postgres using tls")
+		fmt.Println("This above error is expected when connecting to postgres without tls. The program will now connect to postgres using tls.")
 	}
 	defer func() {
 		if db != nil {
