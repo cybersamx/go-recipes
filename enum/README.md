589caeb80f6896a019eae751444eb8cac4da4c10
Samuel Chow <cybersamx@gmail.com>
2023-07-29 15:14:56 -0700

    Add stringer method to enum example

--- enum/README.md
+++ enum/README.md
@@ -8,3 +8,19 @@ This example includes a method for returning the enum value as string. In the `S
 string values and use the enum value, which is an integer type to index the actual string value.
 
 See [Stackoverflow: how to make go print enum fields as string](https://stackoverflow.com/questions/41480543/how-to-make-go-print-enum-fields-as-string/62291060#62291060) for details. You can also implement the method using `switch` to return the string values per Rishabh's comment.
+
+## Using Stringer
+
+We can use also use a codegen tool `stringer` to generate the method `String()` of an enum type definition for us. See the type `MyEnumValImplict`. We need to do the following:
+
+```shell
+go install golang.org/x/tools/cmd/stringer@latest
+go generate
+go run main.go myenumimplicit_string.go
+```
+
+Or simply run `make run`.
+
+## References
+
+* [Stringer](https://pkg.go.dev/golang.org/x/tools/cmd/stringer)
--- enum/main.go
+++ enum/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-// Enums in Go
+// Enums in Go. Explicitly define the String() method to convert a enum value to a string value.
 
 type MyEnum int
 
@@ -14,6 +15,8 @@ const (
 	MyEnumVal3
 )
 
+var errMyEnumParse = errors.New("can't parse enum")
+
 var strs = [...]string{"MyEnumVal1", "MyEnumVal2", "MyEnumVal3"}
 
 // String returns the enum values as string.
@@ -21,6 +24,40 @@ func (me MyEnum) String() string {
 	return strs[me]
 }
 
+func ParseMyEnum(text string) (MyEnum, error) {
+	for i, str := range strs {
+		if str == text {
+			return MyEnum(i), nil
+		}
+	}
+
+	return MyEnum(0), errMyEnumParse
+}
+
+// The implicit way by using go:generate.
+
+type MyEnumImplicit int
+
+//go:generate stringer -type=MyEnumImplicit
+const (
+	MyEnumValImplicit1 MyEnumImplicit = iota
+	MyEnumValImplicit2
+	MyEnumValImplicit3
+)
+
 func main() {
 	fmt.Println(MyEnumVal1, MyEnumVal2, MyEnumVal3)
+
+	texts := []string{"MyEnumVal1", "MyEnumVal2", "MyEnumVal3", "NonExistEnum"}
+	for _, text := range texts {
+		val, err := ParseMyEnum(text)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println(val)
+	}
+
+	fmt.Println(MyEnumValImplicit1, MyEnumValImplicit2, MyEnumValImplicit3)
 }
